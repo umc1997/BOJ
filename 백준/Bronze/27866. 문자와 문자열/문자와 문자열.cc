d71bb093c5280f74288d370dcfdb0cd98b1dddc6
umc1997 <umc1997@naver.com>
2023-03-10 23:19:32 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <set>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// input & assignment
+	string s;
+	cin >> s;
+	int i;
+	cin >> i;
+	cout << s[i - 1] << endl;
+	return 0;
+}
