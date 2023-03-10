e5a624ed681f7c980312dd20dd100665586f41ca
umc1997 <umc1997@naver.com>
2023-03-10 23:16:35 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <stack>
+
+using namespace std;
+int N;
+void print(int d)
+{
+	if (d > N) return;
+	string prefix = "";
+	for (int i = 0; i < d; i++)
+		prefix += "____";
+	cout << prefix << "\"재귀함수가 뭔가요?\"\n";
+	if(d == N)
+		cout << prefix << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
+	else {
+		cout << prefix << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+		cout << prefix << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+		cout << prefix << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+		print(d + 1);
+	}
+	cout << prefix << "라고 답변하였지.\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
+	print(0);
+
+	return 0;
+}
