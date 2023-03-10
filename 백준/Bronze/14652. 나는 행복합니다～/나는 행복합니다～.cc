506ab85efa4846cc9b21f739913e1126ebb3447e
umc1997 <umc1997@naver.com>
2023-03-10 23:10:47 +0900

    [Bronze V] Title: 나는 행복합니다~, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	
+	unsigned int N, M, K;
+	cin >> N >> M >> K;
+	cout << K / M << " " << K % M << "\n";
+	return 0;
+}
