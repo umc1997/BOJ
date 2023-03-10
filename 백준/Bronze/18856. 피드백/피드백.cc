555840c1af2d03d5e6edd4894f428b29c34709ce
umc1997 <umc1997@naver.com>
2023-03-10 23:17:32 +0900

    [Bronze III] Title: 피드백, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18856. 피드백/피드백.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	cout << N << '\n';
+	for (int i = 1; i < N; i++)
+	{
+		cout << i << " ";
+	}
+	cout << 53 << endl;
+	return 0;
+}
