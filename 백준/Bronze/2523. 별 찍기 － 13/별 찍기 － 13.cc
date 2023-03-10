cc044e1386d2d32e9fc4c8fe8fd755662a8e1f6e
umc1997 <umc1997@naver.com>
2023-03-10 22:40:06 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	unsigned int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 0; j < i; j++)
+			cout << "*";
+		cout << "\n";
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 0; j < N-i; j++)
+			cout << "*";
+		cout << "\n";
+	}
+}
