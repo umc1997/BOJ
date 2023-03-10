fe44ab114d1a81101c8cf22274f6ca8a81389475
umc1997 <umc1997@naver.com>
2023-03-10 22:51:32 +0900

    [Bronze II] Title: 만취한 상범, Time: 0 ms, Memory: 1996 KB -BaekjoonHub

---
+++ 백준/Bronze/6359. 만취한 상범/만취한 상범.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	unsigned int T;
+	cin >> T;
+	while (T--)
+	{
+		int a;
+		cin >> a;
+		cout << int(sqrt(a)) << "\n";
+	}
+	return 0;
+}
