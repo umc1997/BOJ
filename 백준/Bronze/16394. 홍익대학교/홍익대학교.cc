395adfd60aaa3f21a0fa4cfe36531ad0c6c9cc8f
umc1997 <umc1997@naver.com>
2023-03-10 23:14:54 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	long long A;
+	cin >> A;
+	cout << A - 1946 << "\n";
+    return 0;
+}
+
