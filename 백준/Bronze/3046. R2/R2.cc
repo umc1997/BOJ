a01fc71d55cac88d101e43f466b77e7af06eca9c
umc1997 <umc1997@naver.com>
2023-03-10 22:46:50 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	cout << 2 * M - N << "\n";
+	return 0;
+}
