626811d2cb9f69ed171bbac33905d45af723e5e1
umc1997 <umc1997@naver.com>
2023-03-10 23:12:54 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+int arr[1010];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	bool a;
+	cin >> a;
+	if (a)
+		cout << "Leading the Way to the Future" << "\n";
+	else
+		cout << "YONSEI" << "\n";
+	return 0;
+}
