ddf401db603be02aed41903bd1818eaf8a09abe2
umc1997 <umc1997@naver.com>
2023-03-10 23:05:40 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	unsigned int N;
+	cin >> N;
+	vector<long long> arr;
+	unsigned int MAX = 200100;
+	for (int i = 0; i < N; i++)
+	{
+		long long x, y;
+		cin >> x >> y;
+		x += 100000;
+		y += 100000;
+		arr.push_back(x + y * MAX);
+	}
+	sort(arr.begin(), arr.end());
+	for (int i = 0; i < N; i++)
+	{
+		long long x1, y1;
+		x1 = arr[i] % MAX;
+		y1 = arr[i] / MAX;
+		x1 -= 100000;
+		y1 -= 100000;
+		cout << x1 << " " << y1 << "\n";
+	}
+}
