4512bb7a22f4e24874339c6e4dc575761b7f41d7
umc1997 <umc1997@naver.com>
2023-03-10 22:38:30 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	unsigned int N;
+	cin >> N;
+	for (int i = N - 1; i >= 0; i--)
+	{
+		string line = "";
+		for (int j = 0; j < N - i - 1; j++)
+			line += " ";
+		for (int j = 0; j < i * 2 + 1; j++)
+			line += "*";
+		cout << line << "\n";
+	}
+	
+	return 0;
+}
+
