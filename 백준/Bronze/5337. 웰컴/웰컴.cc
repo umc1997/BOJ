97ccb40a08626f8668b444746088e758829cd287
umc1997 <umc1997@naver.com>
2023-03-10 22:49:24 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string output = ".  .   .\n\|  \| _ \| _. _ ._ _  _\n\|/\\\|(/.\|(_.(_)[ \| )(/.";
+	
+	cout << output << "\n";
+	return 0;
+}
+
