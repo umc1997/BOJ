0970aee3c27241aa763ff72061e521262e214211
umc1997 <umc1997@naver.com>
2023-03-10 23:03:04 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	unsigned int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		unsigned int A, B;
+		cin >> A >> B;
+		cout << "Case #" << i << ": " << A << " + " << B << " = " << A + B << "\n";
+	}
+}
