fce6f03d105ab5ceed23c856bdd926c23a64d948
umc1997 <umc1997@naver.com>
2023-03-10 23:10:40 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cout << "비와이" << "\n";
+}
+
