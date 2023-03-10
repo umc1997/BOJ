57481ec02432f0b69ee217005765a2d2f51dcd35
umc1997 <umc1997@naver.com>
2023-03-10 22:47:35 +0900

    [Bronze V] Title: 크냐?, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+while True:
+    a, b = map(int, input().rstrip().split(" "))
+    if a == 0 and b == 0:
+        break
+    if a > b:
+        print("Yes")
+    else:
+        print("No")
