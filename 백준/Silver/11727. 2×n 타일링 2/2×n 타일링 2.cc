6a8bf27c1615c125632d07ae70fc9ab8b818f68d
umc1997 <umc1997@naver.com>
2023-03-10 23:07:24 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int DP[1010] = {-1,};
+	int n=0;
+	DP[0] = 1;
+	DP[1] = 1;
+	cin >> n;
+	for (int i = 2; i <= n; i++)
+	{
+		DP[i] = (DP[i - 1] + 2 * DP[i - 2]) % 10007;
+	}
+	cout << DP[n] % 10007<<endl;
+	return 0;
+
+	
+}
