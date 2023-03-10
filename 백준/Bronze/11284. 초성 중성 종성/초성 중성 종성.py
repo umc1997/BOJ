b34c5ade6c47b9c44c288d964fb86b5a3f1bd701
umc1997 <umc1997@naver.com>
2023-03-10 23:04:26 +0900

    [Bronze II] Title: 초성 중성 종성, Time: 140 ms, Memory: 32740 KB -BaekjoonHub

---
+++ 백준/Bronze/11284. 초성 중성 종성/초성 중성 종성.py
@@ -0,0 +1,17 @@
+import re
+BASE_CODE, CHOSUNG, JUNGSUNG = 44032, 588, 28
+
+chosung_list = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+
+jungsung_list = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ",  "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
+
+jongsung_list = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+
+char = input()
+code = ord(char) - BASE_CODE
+char1 = int(code/CHOSUNG)
+char2 = int((code-CHOSUNG*char1)/JUNGSUNG)
+char3 = int(code - (CHOSUNG * char1) - (JUNGSUNG * char2))
+print(chosung_list[char1])
+print(jungsung_list[char2])
+print(jongsung_list[char3])
