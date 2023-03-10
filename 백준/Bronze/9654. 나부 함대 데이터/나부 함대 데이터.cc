98e64cfefefba34b258550a56a74172fe38a3875
umc1997 <umc1997@naver.com>
2023-03-10 22:56:08 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	
+	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n";
+	cout << "N2 Bomber      Heavy Fighter  Limited    21        \n";
+	cout << "J-Type 327     Light Combat   Unlimited  1         \n";
+	cout << "NX Cruiser     Medium Fighter Limited    18        \n";
+	cout << "N1 Starfighter Medium Fighter Unlimited  25        \n";
+	cout << "Royal Cruiser  Light Combat   Limited    4         \n";
+	return 0;
+}
