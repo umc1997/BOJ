e9abbd564996aa2a32cbf3c007e3eaf8adae11ea
umc1997 <umc1997@naver.com>
2023-03-10 23:05:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);	
+
+	char n;
+	cin >> n;
+	int result = n;
+	cout << result << endl;
+}
