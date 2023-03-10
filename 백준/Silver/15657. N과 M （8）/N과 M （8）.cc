8d2d742db5b45632849eabd266dab4797c90a141
umc1997 <umc1997@naver.com>
2023-03-10 23:12:34 +0900

    [Silver III] Title: N과 M (8), Time: 100 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+int n, m;
+int num[10];
+int input[10];
+void dfs(int cnt)
+{
+	if (cnt == m)
+	{
+		for (int i = 0; i < m - 1; i++)
+		{
+			if (num[i] > num[i + 1])
+				return;
+		}
+		for (int i = 0; i < m; i++)
+			cout << num[i] << " ";
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		num[cnt] = input[i];
+		dfs(cnt + 1);
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++)
+		cin >> input[i];
+	
+	sort(input + 1, input + n + 1);
+
+	dfs(0);
+
+	return 0;
+}
