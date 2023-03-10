e4662da045802a247fe9a82a9d734d7ea249643f
umc1997 <umc1997@naver.com>
2023-03-10 22:54:54 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6008 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+
+using namespace std;
+int dp[1010][1010];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string a, b;
+	cin >> a >> b;
+	for (int i = 0; i <= a.size(); i++)
+	{
+		for (int j = 0; j <= b.size(); j++)
+			dp[i][j] = 0;
+	}
+	
+	for (int i = 1; i <= a.size(); i++)
+	{
+		for (int j = 1; j <= b.size(); j++)
+		{
+			if (a[i - 1] == b[j - 1])
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+	cout << dp[a.size()][b.size()] << "\n";
+	return 0;	
+}
