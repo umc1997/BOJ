598c2e0fcf3881819838b1483683445290018a20
umc1997 <umc1997@naver.com>
2023-03-10 23:04:45 +0900

    [Bronze IV] Title: !밀비 급일, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string line;
+	while (true)
+	{
+		getline(cin,line);
+		if (line == "END") break;
+		reverse(line.begin(), line.end());
+		cout << line << "\n";
+	}
+
+	return 0;
+}
