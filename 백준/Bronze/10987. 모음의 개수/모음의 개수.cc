df8f3faddfa495e88239331e3fd3637be03aeb90
umc1997 <umc1997@naver.com>
2023-03-10 23:01:42 +0900

    [Bronze III] Title: 모음의 개수, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string s;
+	cin >> s;
+	int sum = 0;
+	int size = s.size();
+	for (int i = 0; i < size; i++)
+	{
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+			sum++;
+	}
+	cout << sum << "\n";
+	return 0;
+}
