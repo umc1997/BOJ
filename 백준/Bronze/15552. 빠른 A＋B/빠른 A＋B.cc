c18b17ed6384bc304902815f63273ce03a1bd6fb
umc1997 <umc1997@naver.com>
2023-03-10 23:11:50 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 236 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		int A, B;
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+}
