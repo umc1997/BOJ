3232ebcbcac160a603667e2fe57ada56c602d4d7
umc1997 <umc1997@naver.com>
2023-03-10 22:49:30 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cout << "       _.-;;-._\n";
+	cout << "\'-..-\'\|   \|\|   \|\n";
+	cout << "\'-..-\'\|_.-;;-._\|\n";
+	cout << "\'-..-\'\|   \|\|   \|\n";
+	cout << "\'-..-\'\|_.-\'\'-._\| ";
+	return 0;
+}
+
