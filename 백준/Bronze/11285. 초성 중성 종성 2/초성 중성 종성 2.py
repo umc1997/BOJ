f95571752d53ad6eaff30f3bfa8aa94570d30765
umc1997 <umc1997@naver.com>
2023-03-10 23:04:32 +0900

    [Bronze I] Title: 초성 중성 종성 2, Time: 128 ms, Memory: 32728 KB -BaekjoonHub

---
+++ 백준/Bronze/11285. 초성 중성 종성 2/초성 중성 종성 2.py
@@ -0,0 +1,14 @@
+import re
+BASE_CODE, CHOSUNG, JUNGSUNG = 44032, 588, 28
+
+chosung_list = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+
+jungsung_list = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ",  "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
+
+jongsung_list = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+
+char1 = int(chosung_list.index(input()))
+char2 = int(jungsung_list.index(input()))
+char3 = int(jongsung_list.index(input()))
+code = (char1*21 + char2) *28 + char3 + BASE_CODE
+print(chr(code))
