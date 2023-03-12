d07690f70d7273915ecbf9ba45d3ff292de3ed72
umc1997 <umc1997@naver.com>
2023-03-12 23:42:42 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	// input & assignment
+	int N;
+	cin >> N;
+	cout << (long long) N * N * N  << '\n' << 3 << '\n';
+	
+	// output
+
+	return 0;
+}
