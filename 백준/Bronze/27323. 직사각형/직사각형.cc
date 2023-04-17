df58ddac3e48efed2ee4454963bd0f35a5c14c72
umc1997 <umc1997@naver.com>
2023-04-17 15:57:44 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	// input & assignment
+	int A, B;
+	cin >> A >> B;
+	cout << A * B << '\n';
+
+	return 0;
+}
