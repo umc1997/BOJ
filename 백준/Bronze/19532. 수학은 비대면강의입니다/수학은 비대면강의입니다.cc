c7840b0903b7d8add3fa4ad0c57677863fb45951
umc1997 <umc1997@naver.com>
2023-04-17 16:07:07 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	// input & assignment
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	for (int x = -999; x <= 999; x++) {
+		for (int y = -999; y <= 999; y++) {
+			if (a * x + b * y == c && d * x + e * y == f) {
+				cout << x << " " << y << '\n';
+				break;
+			}
+		}
+	}
+	return 0;
+}
